bad6519f2cb7fcc137f4237396cc5e35fb15f9bb
Erwin Bejsta <helioz2000@gmail.com>
2021-03-11 22:33:22 +1100

    moved board 2 to meshed au

--- SODAQ_TTNmapper/SODAQ_TTNmapper.ino
+++ SODAQ_TTNmapper/SODAQ_TTNmapper.ino
@@ -58,17 +58,17 @@ decoded.latitude = ((bytes[0]<<16)>>>0) + ((bytes[1]>>8)>>>0) + bytes[2];
 
 /* Erwin's board id sodaq_one_1 */
 // device EUI: 0004A30B001A26CA
-// ABP details
+// ABP details for asia-se handler registration
 //const char *devAddr = "26041B13";
 //const char *nwkSKey = "31B51E36443F398B11E31018BF5F7E95";
 //const char *appSKey = "1F762E517C4C0CE74EED902260ECB1A7";
 
 /* Erwin's board id sodaq_one_2 */
 // device EUI: 0004A30B001A271C
-// ABP details
-const char *devAddr = "2604102F";
-const char *nwkSKey = "8D440FCDFD5A1A3FA79B4D1DF651485E";
-const char *appSKey = "A794DB9234E2E712EAFA771BF91F202C";
+// ABP details for meshed AU handle registration
+const char *devAddr = "260012F6";
+const char *nwkSKey = "B94B2A32170E90E2E50E920D6CE32A8C";
+const char *appSKey = "1F09ACF5332A14B8C258388277BB8234";
 
 /* Erwin's board id sodaq_one_02 
 const char *devAddr = "26041E61";
